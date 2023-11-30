7fbb7243919a0d077781893400027d70a60e2b1c
rararaneko <xiangzhiluqi@gmail.com>
2023-11-30 09:50:14 +0900

    aaa

---
+++ MyGitProject2D/src/main/java/com/example/demo/controller/MergeHellController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+public class MergeHellController {
+	public static void main(String[] args) {
+		kadaiA();
+	}
+
+	static void kadaiA() {
+		for(int i = 0; i < 10; i++) {
+			System.out.println("現在" + i + "回目のループです。");
+		}
+	}
+}
